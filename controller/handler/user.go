630e8133ba792f25201557af89b20469d287f6fc
Alexander Kevin <alexander.kevin@shortlyst.ai>
2023-08-03 23:08:18 +0700

    Add handler for checkUsername

---
+++ controller/handler/user.go
@@ -0,0 +1,53 @@
+package handler
+
+import (
+	"bettersocial/container"
+	"bettersocial/controller/response"
+	"bettersocial/helper"
+	"bettersocial/model"
+	"bettersocial/usecase"
+
+	"errors"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/go-ozzo/ozzo-validation/v4/is"
+)
+
+type User struct {
+	appContainer *container.Container
+}
+
+func NewUser(appContainer *container.Container) *User {
+	return &User{appContainer: appContainer}
+}
+
+func (u *User) CheckUsername(c *gin.Context) {
+	logger := helper.GetLogger(c).WithField("method", "Controller.Handler.CheckUsername")
+
+	// Get username from path
+	username := c.Param("username")
+
+	if err := validation.Validate(username, is.Alphanumeric); err != nil {
+		response.WriteFailResponse(c, http.StatusBadRequest, errors.New("username should be an alphanumeric"))
+		return
+	}
+
+	// Action
+	userUseCase := usecase.NewUser(u.appContainer)
+	err := userUseCase.CheckUsername(c, username)
+	if err != nil {
+		var e model.Error
+		if !errors.As(err, &e) {
+			logger.WithError(err).Warning("error get username")
+			response.WriteFailResponse(c, http.StatusInternalServerError, err)
+		} else {
+			response.WriteFailResponse(c, e.Code, e)
+		}
+		return
+	}
+
+	response.WriteSuccessResponse(c, nil)
+}
+
---
+++ controller/handler/user_test.go
@@ -0,0 +1,92 @@
+package handler_test
+
+import (
+	"bettersocial/container"
+	"bettersocial/controller/response"
+	"bettersocial/helper/test"
+	"bettersocial/model"
+	"bettersocial/repository/mocks"
+	"encoding/json"
+
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestUser_CheckUsername(t *testing.T) {
+	t.Parallel()
+	t.Run("ShouldReturn200_WhenNotFoundUsername", func(t *testing.T) {
+		t.Parallel()
+		// INIT
+		username := "username"
+		userMock := &mocks.User{}
+		userMock.On("GetByUsername", mock.Anything, username).Return(nil, model.NewNotFoundError()).Once()
+
+		router := test.SetupHttpHandler(t, func(appContainer *container.Container) *container.Container {
+			appContainer.SetTodoRepo(userMock)
+			return appContainer
+		})
+
+		// CODE UNDER TEST
+		w, err := performRequest(router, "GET", "/v1/user/"+username, nil, nil, nil)
+		require.NoError(t, err)
+		defer printOnFailed(t)(w.Body.String())
+
+		// EXPECTATION
+		require.Equal(t, http.StatusOK, w.Code)
+
+		userMock.AssertExpectations(t)
+	})
+
+	t.Run("ShouldReturnErrorConflict_WhenGetByUsernameReturnNoError", func(t *testing.T) {
+		t.Parallel()
+		// INIT
+		username := "username"
+		userMock := &mocks.User{}
+		userMock.On("GetByUsername", mock.Anything, username).Return(&model.User{}, nil).Once()
+
+		router := test.SetupHttpHandler(t, func(appContainer *container.Container) *container.Container {
+			appContainer.SetTodoRepo(userMock)
+			return appContainer
+		})
+
+		// CODE UNDER TEST
+		w, err := performRequest(router, "GET", "/v1/user/"+username, nil, nil, nil)
+		require.NoError(t, err)
+		defer printOnFailed(t)(w.Body.String())
+
+		// EXPECTATION
+		require.Equal(t, http.StatusConflict, w.Code)
+
+		resBody := response.ErrorResponse{}
+		err = json.NewDecoder(w.Body).Decode(&resBody)
+		require.NoError(t, err)
+
+		userMock.AssertExpectations(t)
+	})
+
+	t.Run("ShouldReturnErrorBadRequest_WhenUsernameIsNotAlphanumeric", func(t *testing.T) {
+		t.Parallel()
+		// INIT
+		username := "username-1"
+
+		router := test.SetupHttpHandler(t, func(appContainer *container.Container) *container.Container {
+			return appContainer
+		})
+
+		// CODE UNDER TEST
+		w, err := performRequest(router, "GET", "/v1/user/"+username, nil, nil, nil)
+		require.NoError(t, err)
+		defer printOnFailed(t)(w.Body.String())
+
+		// EXPECTATION
+		require.Equal(t, http.StatusBadRequest, w.Code)
+
+		resBody := response.ErrorResponse{}
+		err = json.NewDecoder(w.Body).Decode(&resBody)
+		require.NoError(t, err)
+
+		require.Equal(t, "username should be an alphanumeric", resBody.Message)
+	})
